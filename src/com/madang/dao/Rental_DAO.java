0f73e302b08ad129b64e7f37b229d722b75ced97
SIST <SIST@DESKTOP-25JNQFA>
2019-11-06 09:33:28 +0900

    세희 대관테이블(rental) 관련 dao, vo, service 파일 생성

---
+++ src/com/madang/dao/Rental_DAO.java
@@ -0,0 +1,50 @@
+package com.madang.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Rental_DAO {
+	//Field
+	String url="jdbc:oracle:thin:@211.63.89.214:1521";
+	String user="madang";
+	String pass="1234";
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	
+	//Constructor
+	public Rental_DAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn=DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {e.printStackTrace();}
+	}
+	
+	//Method
+	public void getPreparedStatement(String sql) {
+		try {
+			pstmt=conn.prepareStatement(sql);
+		} catch (Exception e) {e.printStackTrace();}
+	}
+	
+	/** 리스트 출력 */
+	
+	
+	
+	
+	/** 상세내역 출력 */
+	
+	
+	
+	
+	public void close() {
+		try {
+			if(rs!=null) rs.close();
+			if(pstmt!=null) pstmt.close();
+			if(conn!=null) conn.close();
+		} catch (Exception e) {e.printStackTrace();}
+	}
+}
---
+++ src/com/madang/service/Rental_Service.java
@@ -0,0 +1,10 @@
+package com.madang.service;
+
+public class Rental_Service {
+	/** 리스트 출력 */
+	
+	
+	/** 상세내역 출력 */
+	
+	
+}
